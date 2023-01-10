46d97a3c70574dea4a4ed95c5d315741034f06b7
evan-m-jackson <90336648+evan-m-jackson@users.noreply.github.com>
2023-01-10 13:47:40 -0500

    Add hooks to Todo function to get todo list json

--- src/Todo.tsx
+++ src/Todo.tsx
@@ -1,6 +1,34 @@
-import React, { useState } from 'react';
+import React, { Component, useEffect, useState } from 'react';
+
 
 function Todo() {
+    const [data, setData] = React.useState(null);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(null);
+
+    useEffect(() => {
+        fetch('http://127.0.0.1:8000/todo-list/')
+        .then((response) => {
+            if (!response.ok) {
+                throw new Error(
+                    `This is an HTTP error: The status is ${response.status}`
+                )
+            }
+            return response.json()
+        })
+        .then((actualData) => {
+            setData(actualData)
+            setError(null)
+            console.log(data)
+        })
+        .catch((err) => {
+            setError(err.message)
+            setData(null)
+        })
+        .finally(() => {
+            setLoading(false);
+        })
+    }, [])
 
     return (
         <div>
@@ -9,6 +37,7 @@ function Todo() {
                 <input></input>
                 <button>Add</button>
             </form>
+            <p>{}</p>
         </div>
     )
 }
